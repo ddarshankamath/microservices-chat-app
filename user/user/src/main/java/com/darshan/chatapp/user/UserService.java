cf4ecc465b18b87dd0c9ec215e28aabf0c6351c7
Darshan D Kamath <ddarshankamath@gmail.com>
2025-04-08 22:40:48 +0530

    service annotation added

--- user/user/src/main/java/com/darshan/chatapp/user/UserService.java
+++ user/user/src/main/java/com/darshan/chatapp/user/UserService.java
@@ -1,7 +1,12 @@
 package com.darshan.chatapp.user;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
     private final JWTUtil jwtUtil;
